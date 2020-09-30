ae509342e9b37c775ac84a27738a3dc62fdabcea
belkadihassan <72108250+belkadihassan@users.noreply.github.com>
2020-09-30 17:17:48 +0100

    Add files via upload

---
+++ coding-test/js/test.js
@@ -0,0 +1,5 @@
+const hamburger = document.querlySelector('.hamburger')
+const navbar = document.querlySelector('.navbar')
+const links = document.querlySelectorAll('.navlist li a')
+const navlist =document.querlySelector('.navlist')
+
